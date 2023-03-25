231508938c724711b036b8c48f58985e1c15fc5d
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2023-03-25 12:03:38 +0200

    main

--- src/db/index.ts
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const { USER_DB, PASSWORD_DB, HOST, PORT_DB } = process.env;
-
-export const sequelize = new Sequelize("postApp", "postgres", "haobanjia", {
-  host: HOST,
-  port: parseFloat(PORT_DB!),
-  dialect: "postgres",
-});
